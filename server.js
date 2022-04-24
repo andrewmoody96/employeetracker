8b00233e78f9293d0bf1a12aa5172e7ef82c3488
Andrew Moody <77068588+andrewmoody96@users.noreply.github.com>
2022-04-23 22:02:41 -0500

    Haven't had much time to work on this. Going to come back to this asap.

--- server.js
+++ server.js
@@ -19,42 +19,22 @@ const db = mysql.createConnection(
   console.log("Connected to the employees_db database.")
 );
 
-// Queries database to show all of the departments.
-app.get("/api/departments", (req, res) => {
-  db.query("SELECT * FROM departments", function (err, results) {
-    if (err) throw err;
-    else {
-      res.json(results);
+const questions = [
+  {
+    type: "list",
+    name: "whatFirst",
+    message: "What would you like to do?",
+    // TODO -- Add more options for adding things later.
+    choices: ["View All Departments", "View All Roles", "View All Employees"],
+  },
+];
+
+function businessManagement() {
+  inquirer.prompt(questions).then((answers) => {
+    if (answers.whatFirst === 'View All Departments') {
+      db.query('SELECT * FROM departments', function (err, results) {
+        console.log(results);
+      })
     }
-  });
-});
-
-// Queries database to show all of the roles.
-app.get("/api/roles", (req, res) => {
-  db.query("SELECT * FROM roles", function (err, results) {
-    if (err) throw err;
-    else {
-      res.json(results);
-    }
-  });
-});
-
-// Queries database to show all of the employees.
-app.get("/api/employees", (req, res) => {
-  db.query("SELECT * FROM employees", function (err, results) {
-    if (err) throw err;
-    else {
-      res.json(results);
-    }
-  });
-});
-
-// Returns 404 for a query failure.
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-// Tells server to listen on our defined PORT
-app.listen(PORT, () => {
-  console.log(`server listening at http://localhost:${PORT}`);
-});
+  })
+}
